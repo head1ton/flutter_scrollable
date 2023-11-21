486a3779a855c7eb8c2c5e48d9a40a046090d5dd
head1ton <head1ton@gmail.com>
2023-11-22 01:28:32 +0900

    listView 추가

--- lib/screen/list_view_screen.dart
+++ lib/screen/list_view_screen.dart
@@ -11,7 +11,55 @@ class ListViewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MainLayout(
       title: 'ListViewScreen',
-      body: renderBuilder(),
+      body: ListView.separated(
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return renderContainer(
+            color: rainbowColors[index % rainbowColors.length],
+            index: index,
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          index += 1;
+          // 배너 보여주기 (유용함)
+          if (index % 5 == 0) {
+            return renderContainer(
+              color: Colors.black,
+              index: index,
+              height: 100,
+            );
+          }
+
+          return SizedBox(
+            height: 10.0,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget renderSeparated() {
+    return ListView.separated(
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        index += 1;
+        // 배너 보여주기 (유용함)
+        if (index % 5 == 0) {
+          return renderContainer(
+            color: Colors.black,
+            index: index,
+            height: 100,
+          );
+        }
+
+        return Container();
+      },
     );
   }
 
@@ -43,11 +91,12 @@ class ListViewScreen extends StatelessWidget {
   Widget renderContainer({
     required Color color,
     required int index,
+    double? height,
   }) {
     print(index);
 
     return Container(
-      height: 300,
+      height: height ?? 300,
       color: color,
       child: Text(
         index.toString(),
